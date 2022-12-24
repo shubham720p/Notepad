3767b0072acfdb9723e4d4ac79d3f69f10662d8e
shubham720p <shubhambhosle556@gmail.com>
2022-12-24 19:31:22 +0530

    added print function

--- notepad.py
+++ notepad.py
@@ -1,5 +1,6 @@
 
 import os
+import tempfile
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter.messagebox import showinfo
@@ -26,6 +27,11 @@ def openFile():
         TextArea.insert(1.0, f.read())
         f.close()
 
+# def print_file(txt):
+#     temp_file = tempfile.mkdtemp('.txt')
+#     open(temp_file, "w").write('txt')
+#     os.startfile(temp_file,'print')
+ 
 
 def saveFile():
     global file
@@ -37,7 +43,6 @@ def saveFile():
             file = None
 
         else:
-            #Save as a new file
             f = open(file, "w")
             f.write(TextArea.get(1.0, END))
             f.close()
@@ -63,61 +68,50 @@ def copy():
 def paste():
     TextArea.event_generate(("<>"))
 
+
 def about():
     showinfo("Notepad", "Notepad by shubham bhosale")
 
 if __name__ == '__main__':
-    #Basic tkinter setup
     root = Tk()
     root.title("Untitled - Notepad")
     root.wm_iconbitmap("note.ico")
     root.geometry("1080x720")
 
-    #Add TextArea
     TextArea = Text(root, font="lucida 13")
     file = None
     TextArea.pack(expand=True, fill=BOTH)
 
-    # Lets create a menubar
     MenuBar = Menu(root)
 
-    #File Menu Starts
     FileMenu = Menu(MenuBar, tearoff=0)
-    # To open new file
     FileMenu.add_command(label="New", command=newFile)
 
-    #To Open already existing file
     FileMenu.add_command(label="Open", command = openFile)
 
-    # To save the current file
 
     FileMenu.add_command(label = "Save", command = saveFile)
     FileMenu.add_separator()
+    # FileMenu.add_command(label = "Print", command = print_file(TextArea.get))
+    # FileMenu.add_separator()
     FileMenu.add_command(label = "Exit", command = quitApp)
     MenuBar.add_cascade(label = "File", menu=FileMenu)
-    # File Menu ends
 
-    # Edit Menu Starts
     EditMenu = Menu(MenuBar, tearoff=0)
-    #To give a feature of cut, copy and paste
     EditMenu.add_command(label = "Cut", command=cut)
     EditMenu.add_command(label = "Copy", command=copy)
     EditMenu.add_command(label = "Paste", command=paste)
 
     MenuBar.add_cascade(label="Edit", menu = EditMenu)
 
-    # Edit Menu Ends
 
-    # Help Menu Starts
     HelpMenu = Menu(MenuBar, tearoff=0)
     HelpMenu.add_command(label = "About Notepad", command=about)
     MenuBar.add_cascade(label="Help", menu=HelpMenu)
 
-    # Help Menu Ends
 
     root.config(menu=MenuBar)
 
-    #Adding Scrollbar using rules from Tkinter lecture no 22
     Scroll = Scrollbar(TextArea)
     Scroll.pack(side=RIGHT,  fill=Y)
     Scroll.config(command=TextArea.yview)
